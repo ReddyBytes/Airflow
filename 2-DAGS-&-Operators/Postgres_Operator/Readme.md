e52e401f312c3da65a19b01dd75dd704262a7e08
ReddyBytes <penchalareddy260@gmail.com>
2024-06-19 12:31:00 +0530

    code : implemented postgres operator

---
+++ 2-DAGS-&-Operators/Postgres_Operator/Readme.md
@@ -0,0 +1,26 @@
+# postgres connection with webserver  
+ __Step : 1__ Go to webserver page > admin > connections  
+
+![](https://lh5.googleusercontent.com/H5RVZ2yZnxvK8j_3RYHOydb04ICXulQy2zYT9NFM7qSFJFoYZ5sa8d3lOR9v5A0AIp9rxKGVES_KL16LcgN-b2_tgOD1ULl3h3QcHVVospJH9fGI4V6ymNxoEceYH9dMqlrs-Xfs)  
+
+ __Step : 2__ Tap on " + " to add new connection  
+
+ ![](https://s3.amazonaws.com/recipes.dezyre.com/use-postgresql-airflow-dag/materials/bigdata_2.jpg)
+
+ __Step : 3__   
+
+ Connection id : postgres_localhost  -----> this should match with [postgres_db.py](/DAGS/Postgres_Operator/postgres_db.py) line 23  
+
+ connection type : Postgres  
+
+ Host : postgres   -----> this should match with [docker compose.yml](/Airflow_Installation/docker-compose.yml) line 90  
+
+
+
+Database, Login and password : same as [docker compose.yml](/Airflow_Installation/docker-compose.yml)  line 95, 93,94
+
+Port : 5432  
+
+![](https://chrischow.github.io/dataandstuff/graphics/2022-01-26-open-options-chains-part-iii/postgres_connection.jpg)  
+
+__step : 4__ Save the Connection
---
+++ 2-DAGS-&-Operators/Postgres_Operator/pg_export_data_file.py
@@ -0,0 +1,51 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+
+
+default_args = {
+    'owner': 'your_name',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=5)
+}
+
+
+with DAG(
+    dag_id='dag_with_postgres_operator_v1',
+    default_args=default_args,
+    start_date=datetime(2021, 12, 19),
+    schedule_interval='0 0 * * *'           # For more information: https://crontab.guru/
+) as dag:
+    
+    
+    task1 = PostgresOperator(
+        task_id='create_postgres_table',
+        postgres_conn_id='postgres_localhost',       # this name and connection id name in airflow webserver >admin >connections> postgres should be same
+        sql="""
+            CREATE TABLE IF NOT EXISTS dag_runs (
+                dt date,
+                dag_id character varying,
+                primary key (dt, dag_id)
+            )
+        """
+    )
+
+    task2 = PostgresOperator(
+        task_id='insert_into_table',
+        postgres_conn_id='postgres_localhost',
+        sql="""
+            INSERT INTO dag_runs (dt, dag_id) values ('{{ ds }}', '{{ dag.dag_id }}')
+        """
+    )
+
+
+    task_export = PostgresOperator(
+        task_id='export_table_to_file',
+        postgres_conn_id='postgres_localhost',
+        sql="""
+            COPY dag_runs TO '/path/to/dag_runs.csv' DELIMITER ',' CSV HEADER;
+        """
+    )
+    
+    task1 >> task2 >> task_export
+
